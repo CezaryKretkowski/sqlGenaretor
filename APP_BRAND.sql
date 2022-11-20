5fd5a63d92b0a704595641c1eeee3208cc4f059c
CezaryKretkowski <73443812+CezaryKretkowski@users.noreply.github.com>
2022-11-20 17:12:42 +0100

    commit

---
+++ APP_BRAND.sql
@@ -0,0 +1,10 @@
+DELETE FROM APP_BRAND;
+
+insert into APP_BRAND values(1,"Gibson","images/Electric_Guitars_gl93EXR.jpg");
+insert into APP_BRAND values(2,"Fender","images/Electric_Guitars_gl93EXR.jpg");
+insert into APP_BRAND values(3,"PRS","images/Electric_Guitars_gl93EXR.jpg");
+insert into APP_BRAND values(4,"G&L","images/Electric_Guitars_gl93EXR.jpg");
+insert into APP_BRAND values(5,"Rickenbacker","images/Electric_Guitars_gl93EXR.jpg");
+insert into APP_BRAND values(6,"Ibanez","images/Electric_Guitars_gl93EXR.jpg");
+insert into APP_BRAND values(7,"ESP","images/Electric_Guitars_gl93EXR.jpg");
+insert into APP_BRAND values(8,"Jackson","images/Electric_Guitars_gl93EXR.jpg");
